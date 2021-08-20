11a53a0e62734b224b9e6520ffd870e3f84eed33
Nikita <nikita.proger21@gmail.com>
2021-08-20 15:08:46 +0300

    Links rendering has been created with using v-for and binding at Sidebar.vue, styles has been updated

---
+++ src/components/app/Sidebar.vue
@@ -0,0 +1,30 @@
+<template>
+  <ul class="sidenav app-sidenav open">
+    <router-link
+      tag="li"
+      active-class="active"
+      :to="link.url"
+      v-for="link in links"
+      :key="link.url"
+      :exact="link.exact"
+    >
+      <a href="#" class="waves-effect waves-teal pointer">{{ link.title }}</a>
+    </router-link>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      links: [
+        { title: "Money Account", url: "/", exact: true },
+        { title: "History", url: "/history" },
+        { title: "Planning", url: "/planning" },
+        { title: "New Entry", url: "/record" },
+        { title: "Categories", url: "/categories" },
+      ],
+    };
+  },
+};
+</script>
