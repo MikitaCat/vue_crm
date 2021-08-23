a01b437d7736fe79178d70f5939ebaa87f808662
Nikita <nikita.proger21@gmail.com>
2021-08-23 15:52:41 +0300

    Add firebase

--- src/main.js
+++ src/main.js
@@ -1,23 +1,43 @@
-import Vue from 'vue'
-import Vuelidate from 'vuelidate'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import dateFilter from './filters/dateFilter'
-import messagePlugin from '@/utils/message.plugin'
-import 'materialize-css/dist/js/materialize'
-import './registerServiceWorker'
+import Vue from "vue";
+import Vuelidate from "vuelidate";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import dateFilter from "./filters/dateFilter";
+import messagePlugin from "@/utils/message.plugin";
+import "materialize-css/dist/js/materialize";
+import "./registerServiceWorker";
 
-Vue.config.productionTip = false
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
 
-Vue.use(Vuelidate)
+Vue.config.productionTip = false;
 
-Vue.use(messagePlugin)
+Vue.use(Vuelidate);
 
-Vue.filter('date', dateFilter)
+Vue.use(messagePlugin);
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+Vue.filter("date", dateFilter);
+
+firebase.initializeApp({
+  apiKey: "AIzaSyA0OFoOm1mH3K7NDKv1Ent4xscSi62Ne04",
+  authDomain: "vue-js--crm.firebaseapp.com",
+  projectId: "vue-js--crm",
+  storageBucket: "vue-js--crm.appspot.com",
+  messagingSenderId: "370893718971",
+  appId: "1:370893718971:web:564c9a9beba66420c3f09e",
+  measurementId: "G-CT2EYEPKEZ",
+});
+
+let app;
+
+firebase.auth().onAuthStateChanged(() => {
+  if (!app) {
+    new Vue({
+      router,
+      store,
+      render: (h) => h(App),
+    }).$mount("#app");
+  }
+});
