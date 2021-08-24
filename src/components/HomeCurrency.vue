58c693c558f7f89e719d35790e9d6be2cbedd35a
Nikita <nikita.proger21@gmail.com>
2021-08-24 14:57:26 +0300

    Decomposition Home component into HomeBill.vue and HomeCurrency.vue

---
+++ src/components/HomeCurrency.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="col s12 m6 l8">
+    <div class="card cyan lighten-2 bill-card">
+      <div class="card-content white-text">
+        <div class="card-header">
+          <span class="card-title">Exchange Rates</span>
+        </div>
+        <table>
+          <thead>
+            <tr>
+              <th>Currency</th>
+              <th>Rate</th>
+              <th>Date</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td>rub</td>
+              <td>12121</td>
+              <td>12.12.12</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,50 +9,20 @@
     </div>
 
     <div class="row">
-      <div class="col s12 m6 l4">
-        <div class="card light-blue bill-card">
-          <div class="card-content white-text">
-            <span class="card-title">Currency account</span>
-
-            <p class="currency-line">
-              <span>12.0 Р</span>
-            </p>
-          </div>
-        </div>
-      </div>
-
-      <div class="col s12 m6 l8">
-        <div class="card cyan lighten-2 bill-card">
-          <div class="card-content white-text">
-            <div class="card-header">
-              <span class="card-title">Exchange Rates</span>
-            </div>
-            <table>
-              <thead>
-                <tr>
-                  <th>Currency</th>
-                  <th>Rate</th>
-                  <th>Date</th>
-                </tr>
-              </thead>
-
-              <tbody>
-                <tr>
-                  <td>rub</td>
-                  <td>12121</td>
-                  <td>12.12.12</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
-      </div>
+      <HomeBill />
+      <HomeCurrency />
     </div>
   </div>
 </template>
 
 <script>
+import HomeBill from "@/components/HomeBill";
+import HomeCurrency from "@/components/HomeCurrency";
 export default {
   name: "home",
+  components: {
+    HomeBill,
+    HomeCurrency,
+  },
 };
 </script>
