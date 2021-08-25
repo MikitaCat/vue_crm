737698f62b0af526d9456467ee1ccc782325abca
Nikita <nikita.proger21@gmail.com>
2021-08-25 12:41:08 +0300

    Added input validation (vuelidate) to CategoryCreate.vue, data has been created at CategoryCreate.vue, submitHandler method and category action nas been created

--- src/components/CategoryCreate.vue
+++ src/components/CategoryCreate.vue
@@ -5,17 +5,35 @@
         <h4>Create</h4>
       </div>
 
-      <form>
+      <form @submit.prevent="submitHandler">
         <div class="input-field">
-          <input id="name" type="text" />
+          <input
+            id="name"
+            type="text"
+            v-model="title"
+            :class="{ invalid: $v.title.$dirty && !$v.title.required }"
+          />
           <label for="name">Title</label>
-          <span class="helper-text invalid">Enter the Title</span>
+          <span
+            v-if="$v.title.$dirty && !$v.title.required"
+            class="helper-text invalid"
+            >Enter the Category Title</span
+          >
         </div>
 
         <div class="input-field">
-          <input id="limit" type="number" />
+          <input
+            id="limit"
+            type="number"
+            v-model.number="limit"
+            :class="{ invalid: $v.limit.$dirty && !$v.limit.minValue }"
+          />
           <label for="limit">Limit</label>
-          <span class="helper-text invalid">Minimum Value</span>
+          <span
+            class="helper-text invalid"
+            v-if="$v.limit.$dirty && !$v.limit.minValue"
+            >Minimum Value {{ $v.limit.$params.minValue.min }}</span
+          >
         </div>
 
         <button class="btn waves-effect waves-light" type="submit">
@@ -26,3 +44,44 @@
     </div>
   </div>
 </template>
+
+<script>
+import { required, minValue } from "vuelidate/lib/validators";
+
+export default {
+  name: "CategoryCreate",
+  data() {
+    return {
+      title: "",
+      limit: 100,
+    };
+  },
+
+  validations: {
+    title: { required },
+    limit: { minValue: minValue(100) },
+  },
+
+  mounted() {
+    //to fix input placeholders
+    M.updateTextFields();
+  },
+
+  methods: {
+    async submitHandler() {
+      if (this.$v.$invalid) {
+        this.$v.$touch();
+        return;
+      }
+
+      try {
+        const category = await this.$store.dispatch("createCategory", {
+          title: this.title,
+          limit: this.limit,
+        });
+        console.log(category);
+      } catch (e) {}
+    },
+  },
+};
+</script>
