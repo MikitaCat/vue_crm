785df5bb4440650fda8c505530851450c43d0cd4
Nikita <nikita.proger21@gmail.com>
2021-08-25 15:06:27 +0300

    Select category at CatgoryEdit.vue has been updated (added validations and select option with output data at select inputs using created() and watch (!)

--- src/components/CategoryEdit.vue
+++ src/components/CategoryEdit.vue
@@ -7,7 +7,7 @@
 
       <form>
         <div class="input-field">
-          <select ref="select">
+          <select ref="select" v-model="current">
             <option
               v-for="category of categories"
               :key="category.id"
@@ -19,15 +19,33 @@
         </div>
 
         <div class="input-field">
-          <input type="text" id="name" />
+          <input
+            id="name"
+            type="text"
+            v-model="title"
+            :class="{ invalid: $v.title.$dirty && !$v.title.required }"
+          />
           <label for="name">Title</label>
-          <span class="helper-text invalid">TITLE</span>
+          <span
+            v-if="$v.title.$dirty && !$v.title.required"
+            class="helper-text invalid"
+            >Enter the Category Title</span
+          >
         </div>
 
         <div class="input-field">
-          <input id="limit" type="number" />
+          <input
+            id="limit"
+            type="number"
+            v-model.number="limit"
+            :class="{ invalid: $v.limit.$dirty && !$v.limit.minValue }"
+          />
           <label for="limit">Limit</label>
-          <span class="helper-text invalid">LIMIT</span>
+          <span
+            class="helper-text invalid"
+            v-if="$v.limit.$dirty && !$v.limit.minValue"
+            >Minimum Value {{ $v.limit.$params.minValue.min }}</span
+          >
         </div>
 
         <button class="btn waves-effect waves-light" type="submit">
@@ -40,6 +58,8 @@
 </template>
 
 <script>
+import { required, minValue } from "vuelidate/lib/validators";
+
 export default {
   name: "CategoryEdit",
 
@@ -53,11 +73,37 @@ export default {
   data() {
     return {
       select: null,
+      title: "",
+      limit: 100,
+      current: null,
     };
+  },
+
+  validations: {
+    title: { required },
+    limit: { minValue: minValue(100) },
+  },
+
+  created() {
+    const { id, title, limit } = this.categories[0];
+    this.current = id;
+    this.title = title;
+    this.limit = limit;
+  },
+
+  watch: {
+    current(catId) {
+      const { title, limit } = this.categories.find((c) => c.id === catId);
+      this.title = title;
+      this.limit = limit;
+    },
   },
 
   mounted() {
     this.select = M.FormSelect.init(this.$refs.select);
+
+    //to fix input placeholders
+    M.updateTextFields();
   },
 
   destroyed() {
